56623d4bd890179d6441b89625d14ec2ab8b1107
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-02 01:19:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,8 +28,8 @@ class Transfer
 
   def reverse_transfer
     if valid? && receiver.balance > amount && self.status == "pending"
-      sender.balance += amount
       receiver.balance -= amount
+      sender.balance += amount
       self.status = "reversed"
     else
       reject_transfer
