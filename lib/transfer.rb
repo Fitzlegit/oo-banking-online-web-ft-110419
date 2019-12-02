8b7f37fb0035170b6ef22f6b15d6e4999090ae67
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-02 00:49:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       receiver.balance += amount
       self.status = "complete"
     else
-      p "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
 end
